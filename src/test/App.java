e0918ae58387248bb95a9e79eae22387aae2bdb0
patilashishp <109000399+patilashishp@users.noreply.github.com>
2022-08-05 17:02:42 +0530

    Update App.java

--- src/test/App.java
+++ src/test/App.java
@@ -1,50 +1,5 @@
 package com.mycompany;
 
-
-
-
-
-package sp.sd.nexusartifactuploader;
-import hudson.EnvVars;
-import hudson.Extension;
-import hudson.Launcher;
-import hudson.model.*;
-import hudson.security.ACL;
-import hudson.tasks.BuildStepDescriptor;
-import hudson.tasks.Builder;
-import hudson.util.FormValidation;
-import hudson.util.Secret;
-import hudson.util.ListBoxModel;
-import hudson.FilePath;
-import hudson.FilePath.FileCallable;
-import hudson.remoting.VirtualChannel;
-import jenkins.MasterToSlaveFileCallable;
-import jenkins.tasks.SimpleBuildStep;
-import org.kohsuke.stapler.AncestorInPath;
-import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.QueryParameter;
-import java.io.File;
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.Collections;
-import javax.annotation.CheckForNull;
-import javax.annotation.Nullable;
-import jenkins.model.Jenkins;
-import org.jenkinsci.remoting.RoleChecker;
-import com.cloudbees.plugins.credentials.CredentialsMatchers;
-import com.cloudbees.plugins.credentials.CredentialsProvider;
-import com.cloudbees.plugins.credentials.common.StandardCredentials;
-import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
-import com.cloudbees.plugins.credentials.common.StandardUsernameListBoxModel;
-import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
-import com.cloudbees.plugins.credentials.domains.DomainRequirement;
-import com.google.common.base.Strings;
-
-
-
-
-
-
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
